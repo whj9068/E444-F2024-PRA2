a6b429e3a9861316869b17c87685a3ef58b6bc6f
whj9068 <lilywang020626@gmail.com>
2024-09-25 22:59:44 -0400

    activity 1.3 completed

---
+++ activity1-3.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
